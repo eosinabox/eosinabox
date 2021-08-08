da4842b3fe420e97c250cf1a093c8ebcec902aa3
Ami Heines <ami.heines@gmail.com>
2021-08-08 22:22:51 +0300

    local test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,20 @@
 'use strict';
 $(()=>{
   console.log('this is the onload function of jquery...');
+  $('#btnTestEsr').on('click', ()=>{
+    console.log('user clicked the test-esr btn...');
+    $.ajax({
+      type: 'POST',
+      url: '/newaccount/testEsr',
+      timeout: 15*1000
+    }).done(res => {
+      console.log('[testEsr] frontend res:', res);
+    })
+    .fail(err => {
+      console.log('[testEsr] frontend err:', err);
+    });
+
+  });
   $('#btnCheckAvailability').on('click', ()=>{
     console.log('user clicked the btn, check availability of the account name using the backend of this server...');
     $.ajax({
@@ -8,10 +22,10 @@ $(()=>{
       url: '/newaccount/isavailable/' + $('#accountname').val(),
       timeout: 15*1000
     }).done(res => {
-      console.log('frontend res:', res);
+      console.log('[isavailable] frontend res:', res);
     })
     .fail(err => {
-      console.log('frontend err:', err);
+      console.log('[isavailable] frontend err:', err);
     });
   });
 });
